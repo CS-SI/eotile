4415cc1f811b5e6b64dd1655f48496a60249788c
Mathis <germa.mathis@gmail.com>
2021-02-15 19:01:56 +0100

    Reformating using black & isort

--- setup.py
+++ setup.py
@@ -1,34 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-
-    name='eotile',  
-    
-    version='0.0.1',
-
-    description='Managed Sentinel-2 and Landsat8 tile', 
-
-    author='Mickaël Savinaud, Mathis A. Germa',
-
-    author_email='mickael.savinaud@csgroup.eu, mathis.germa@csgroup.eu', 
+    name="eotile",
+    version="0.0.1",
+    description="Managed Sentinel-2 and Landsat8 tile",
+    author="Mickaël Savinaud, Mathis A. Germa",
+    author_email="mickael.savinaud@csgroup.eu, mathis.germa@csgroup.eu",
     license="Copyright (c) 2021 CS Group, Tous droits réservés",
-
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    python_requires='>=3.6, <4',
-
-    install_requires=['GDAL'],
-
-    extras_require={ 
-        'dev': ['check-manifest', 'geopandas'],
-        'test': ['coverage'],
+    python_requires=">=3.6, <4",
+    install_requires=["GDAL"],
+    extras_require={
+        "dev": ["check-manifest", "geopandas"],
+        "test": ["coverage"],
     },
-
-    
-    entry_points={  
-        'console_scripts': [
-            'eotile=eotile.eotile_cli:main',
+    entry_points={
+        "console_scripts": [
+            "eotile=eotile.eotile_cli:main",
         ],
     },
 )
