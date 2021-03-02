b061056b49c2621686543742438bdba5ab47ed5e
Mathis <germa.mathis@gmail.com>
2021-03-02 10:18:10 +0100

    changed verbosity handling & bi-logging management

--- eotile/eotile_cli.py
+++ eotile/eotile_cli.py
@@ -26,6 +26,32 @@ from eotile.eotiles.eotiles import (
 from eotile.eotiles.get_bb_from_tile_id import get_tiles_from_tile_id
 
 
+def build_logger(level, user_file_name=None):
+    # Creating DEV logger
+    dev_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    dev_handler = logging.FileHandler("dev_log.log")
+    dev_handler.setFormatter(dev_formatter)
+
+    dev_logger = logging.getLogger("dev_logger")
+    dev_logger.setLevel(level)
+    dev_logger.addHandler(dev_handler)
+
+    # Creating User logger
+    user_formatter = logging.Formatter('%(message)s')
+
+    if user_file_name is not None:
+        user_handler = logging.FileHandler(user_file_name)
+    else:
+        user_handler = logging.StreamHandler()
+    user_handler.setFormatter(user_formatter)
+
+    user_logger = logging.getLogger("user_logger")
+    user_logger.setLevel(logging.INFO)
+    user_logger.addHandler(user_handler)
+
+    return dev_logger, user_logger
+
 def build_parser():
     """Creates a parser suitable for parsing a command line invoking this program.
 
@@ -72,12 +98,11 @@ def build_parser():
         "on standard output",
     )
 
-    parser.add_argument(
-        "-log_level",
-        choices=["debug", "info", "warn", "error"],
-        help="Level of the logging system, default is warn",
-    )
+    parser.add_argument("-v", "--verbosity", action="count",
+                        help="Increase output verbosity")
 
+    parser.add_argument("-logger_file",
+                        help="Redirect information from standard output to a file")
     return parser
 
 
@@ -89,16 +114,18 @@ def main(arguments=None):
     """
     arg_parser = build_parser()
     args = arg_parser.parse_args(args=arguments)
-
-    if args.log_level == "debug":
-        logging.basicConfig(filename="eotile_cli.log", level=logging.DEBUG)
-    elif args.log_level == "info":
-        logging.basicConfig(filename="eotile_cli.log", level=logging.INFO)
-    elif args.log_level == "error":
-        logging.basicConfig(filename="eotile_cli.log", level=logging.ERROR)
+    print("verbosity ", args.verbosity) # DEBUG
+    if args.verbosity is None:  # Default
+        log_level = logging.ERROR
+    elif args.verbosity == 1:
+        log_level = logging.WARNING
+    elif args.verbosity == 2:
+        log_level = logging.INFO
+    elif args.verbosity > 2:
+        log_level = logging.DEBUG
     else:
-        logging.basicConfig(filename="eotile_cli.log", level=logging.WARNING)
-    logger = logging.getLogger(__name__)
+        log_level = logging.NOTSET
+    dev_logger, user_logger = build_logger(log_level, args.logger_file)
 
     with open("config/data_path") as conf_file:
         data_path = conf_file.readline()
@@ -130,13 +157,13 @@ def main(arguments=None):
             elif args.input[0] == "file":
                 aoi_filepath = Path(args.input[1])
                 tile_list_s2 = create_tiles_list_s2(filename_tiles_s2, aoi_filepath)
-                logger.info(
+                dev_logger.info(
                     "Nb of S2 tiles which crossing the AOI: {}".format(
                         len(tile_list_s2)
                     )
                 )
             else:
-                logger.error(f"Unrecognized Option : {args.input[0]}")
+                dev_logger.error(f"Unrecognized Option : {args.input[0]}")
 
         if not args.s2_only:
             # L8 Tiles
@@ -157,13 +184,13 @@ def main(arguments=None):
             elif args.input[0] == "file":
                 aoi_filepath = Path(args.input[1])
                 tile_list_l8 = create_tiles_list_l8(filename_tiles_l8, aoi_filepath)
-                logger.info(
+                dev_logger.info(
                     "Nb of L8 tiles which crossing the AOI: {}".format(
                         len(tile_list_l8)
                     )
                 )
             else:
-                logger.error(f"Unrecognized Option : {args.input[0]}")
+                dev_logger.error(f"Unrecognized Option : {args.input[0]}")
 
     # Outputting the result
     if args.to_file is not None:
@@ -181,25 +208,25 @@ def main(arguments=None):
     elif args.to_wkt:
         if len(tile_list_s2) > 0:
             for elt in tile_list_s2:
-                sys.stdout.write("S2 Tile: " + elt.polyBB.wkt + "\n")
+                user_logger.info("S2 Tile: " + elt.polyBB.wkt + "\n")
 
         if len(tile_list_l8) > 0:
             for elt in tile_list_l8:
-                sys.stdout.write("L8 Tile: " + elt.polyBB.wkt + "\n")
+                user_logger.info("L8 Tile: " + elt.polyBB.wkt + "\n")
     elif args.to_bbox:
         if len(tile_list_s2) > 0:
             for elt in tile_list_s2:
-                sys.stdout.write("S2 Tile Bounds: " + str(elt.polyBB.bounds) + "\n")
+                user_logger.info("S2 Tile Bounds: " + str(elt.polyBB.bounds) + "\n")
         if len(tile_list_l8) > 0:
             for elt in tile_list_l8:
-                sys.stdout.write("L8 Tile Bounds: " + str(elt.polyBB.bounds) + "\n")
+                user_logger.info("L8 Tile Bounds: " + str(elt.polyBB.bounds) + "\n")
     elif args.to_tile_id:
         if len(tile_list_s2) > 0:
             for elt in tile_list_s2:
-                sys.stdout.write("S2 Tile id: " + str(elt.ID) + "\n")
+                user_logger.info("S2 Tile id: " + str(elt.ID) + "\n")
         if len(tile_list_l8) > 0:
             for elt in tile_list_l8:
-                sys.stdout.write("L8 Tile id: " + str(elt.ID) + "\n")
+                user_logger.info("L8 Tile id: " + str(elt.ID) + "\n")
     elif args.to_location:
         geolocator = Nominatim(user_agent="EOTile")
         if len(tile_list_s2) > 0:
@@ -208,7 +235,7 @@ def main(arguments=None):
                 centroid.reverse()
                 location = geolocator.reverse(centroid)
                 if location is not None:
-                    sys.stdout.write(str(location) + "\n")
+                    user_logger.info(str(location) + "\n")
 
         if len(tile_list_l8) > 0:
             for elt in tile_list_l8:
@@ -216,15 +243,15 @@ def main(arguments=None):
                 centroid.reverse()
                 location = geolocator.reverse(centroid)
                 if location is not None:
-                    sys.stdout.write(str(location) + "\n")
+                    user_logger.info(str(location) + "\n")
     else:
         if len(tile_list_s2) > 0:
             for elt in tile_list_s2:
-                sys.stdout.write(str(elt))
+                user_logger.info(str(elt))
 
         if len(tile_list_l8) > 0:
             for elt in tile_list_l8:
-                sys.stdout.write(str(elt))
+                user_logger.info(str(elt))
 
 
 if __name__ == "__main__":
