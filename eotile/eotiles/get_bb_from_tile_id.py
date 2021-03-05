3310617e96302ef359618ae049517f25eeccf75e
Mathis <germa.mathis@gmail.com>
2021-03-05 11:24:13 +0100

    Updated get tile id for Copernicus Tiles

--- eotile/eotiles/get_bb_from_tile_id.py
+++ eotile/eotiles/get_bb_from_tile_id.py
@@ -25,8 +25,8 @@ from eotile.eotiles.eotiles import EOTile, S2Tile
 
 
 def get_tiles_from_tile_id(
-    tile_id: str, aux_data_dirpath: Path, s2_only: bool, l8_only: bool, srtm: bool, Copernicus: bool, min_overlap=None
-) -> Tuple[List[S2Tile], List[EOTile], List[EOTile]]:
+    tile_id: str, aux_data_dirpath: Path, s2_only: bool, l8_only: bool, srtm: bool, cop: bool, min_overlap=None
+) -> Tuple[List[S2Tile], List[EOTile], List[EOTile], List[EOTile]]:
     """Returns the bounding box of a tile designated by its ID.
 
     :param tile_id: The identifier of the tile
@@ -51,6 +51,10 @@ def get_tiles_from_tile_id(
     # SRTM Tiles
     filename_tiles_srtm = (
             aux_data_dirpath / "srtm" / "srtm_grid_1deg.shp"
+    )
+    # Copernicus Tiles
+    filename_tiles_cop = (
+            aux_data_dirpath / "Copernicus" / "dem30mGrid.shp"
     )
     check_bb_on_s2, check_bb_on_l8, check_bb_on_srtm = False, False, False
 
@@ -82,6 +86,18 @@ def get_tiles_from_tile_id(
         ):  # In this case, the key does not exist so we output empty
             check_bb_on_srtm = True
 
+    if cop:
+        # Search on Copernicus Tiles
+        tile_list_cop = geom_to_eo_tiles(wkt, None, filename_tiles_cop, "Copernicus")
+        try:
+            output_srtm.append(get_tile(tile_list_cop, tile_id))
+            tile = output_srtm[0]
+        except (
+            KeyError,
+            ValueError,
+        ):  # In this case, the key does not exist so we output empty
+            check_bb_on_cop = True
+
     if not l8_only:
         # Search on s2 Tiles
         tile_list_s2 = geom_to_s2_tiles(wkt, None, filename_tiles_s2)
@@ -106,6 +122,10 @@ def get_tiles_from_tile_id(
         if check_bb_on_srtm:
             output_srtm = create_tiles_list_eo_from_geometry(
                 filename_tiles_srtm, tile.polyBB, "SRTM", min_overlap
+            )
+        if check_bb_on_cop:
+            output_cop = create_tiles_list_eo_from_geometry(
+                filename_tiles_srtm, tile.polyBB, "Copernicus", min_overlap
             )
     except (UnboundLocalError, IndexError):
         return [], [], [], []
