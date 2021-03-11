ac0aea3ba76b4bb06d2bbeaa3e590649909ed996
Mathis Axel Germa <mathis.germa@csgroup.eu>
2021-03-11 11:48:57 +0100

    Improve tile_id research performance

--- eotile/eotiles/get_bb_from_tile_id.py
+++ eotile/eotiles/get_bb_from_tile_id.py
@@ -15,15 +15,47 @@ from eotile.eotiles.eotiles import (
     get_tile,
     get_tile_s2,
     bbox_to_wkt,
-    geom_to_s2_tiles,
-    geom_to_eo_tiles,
+    load_tiles_list_eo,
+    load_tiles_list_s2,
     create_tiles_list_eo_from_geometry,
     create_tiles_list_s2_from_geometry,
 )
 from eotile.eotiles.eotiles import EOTile, S2Tile
 import logging
+import re
 
 dev_logger = logging.getLogger("dev_logger")
+
+
+def tile_id_matcher(input_value: str) -> str:
+    """
+    Induces the type of the input from the user input
+
+    :param input_value: input provided by user of the cli
+    :return: type of the input: wkt, bbox, tile_id, file, location
+    :rtype: str
+    :raises ValueError: when the input value cannot be parsed
+    """
+    cop_srtm_pattern = "(N|S)([0-9]){2,3}(E|W)([0-9]){2,3}"
+
+    s2_pattern = "([0-9]){2}([A-Z]){3}"
+
+    l8_pattern = "([0-9]){4,6}"
+
+    cop_srtm_reg = re.compile(cop_srtm_pattern)
+    s2_reg = re.compile(s2_pattern)
+    l8_reg = re.compile(l8_pattern)
+
+    if cop_srtm_reg.match(input_value) and cop_srtm_reg.match(input_value).string == input_value:
+        return False, False, True, True
+
+    if s2_reg.match(input_value) and s2_reg.match(input_value).string == input_value:
+        return True, False, False, False
+
+    if l8_reg.match(input_value) and l8_reg.match(input_value).string == input_value:
+        return False, True, False, False
+
+    raise ValueError(f"Cannot parse this input: {input_value}")
 
 
 def get_tiles_from_tile_id(
@@ -64,74 +96,48 @@ def get_tiles_from_tile_id(
     filename_tiles_srtm = aux_data_dirpath / "srtm" / "srtm_grid_1deg.shp"
     # Copernicus Tiles
     filename_tiles_cop = aux_data_dirpath / "Copernicus" / "dem30mGrid.shp"
-    check_bb_on_s2, check_bb_on_l8, check_bb_on_srtm, check_bb_on_cop = False, False, False, False
 
-    wkt = bbox_to_wkt(["-90", "90", "-180", "180"])
+    [is_s2, is_l8, is_cop, is_srtm] = tile_id_matcher(tile_id)
     output_s2, output_l8, output_srtm, output_cop = [], [], [], []
     tile = None
-    if not s2_only:
+    if not s2_only and is_l8:
         # Search on l8 Tiles
-        tile_list_l8 = geom_to_eo_tiles(wkt, None, filename_tiles_l8, "L8")
-        try:
-            output_l8.append(get_tile(tile_list_l8, tile_id))
-            tile = output_l8[0]
-        except (
-            KeyError,
-            ValueError,
-        ):  # In this case, the key does not exist so we output empty
-            if not l8_only or srtm or cop:
-                check_bb_on_l8 = True
+        tile_list_l8 = load_tiles_list_eo(filename_tiles_l8, "L8")
+        output_l8.append(get_tile(tile_list_l8, tile_id))
+        tile = output_l8[0]
 
-    if srtm:
+    if srtm and is_srtm:
         # Search on SRTM Tiles
-        tile_list_srtm = geom_to_eo_tiles(wkt, None, filename_tiles_srtm, "SRTM")
-        try:
-            output_srtm.append(get_tile(tile_list_srtm, tile_id))
-            tile = output_srtm[0]
-        except (
-            KeyError,
-            ValueError,
-        ):  # In this case, the key does not exist so we output empty
-            check_bb_on_srtm = True
+        tile_list_srtm = load_tiles_list_eo(filename_tiles_srtm, "SRTM")
+        output_srtm.append(get_tile(tile_list_srtm, tile_id))
+        tile = output_srtm[0]
 
-    if cop:
+    if cop and is_cop:
         # Search on Copernicus Tiles
-        tile_list_cop = geom_to_eo_tiles(wkt, None, filename_tiles_cop, "Copernicus")
-        try:
-            output_cop.append(get_tile(tile_list_cop, tile_id))
-            tile = output_cop[0]
-        except (
-            KeyError,
-            ValueError,
-        ):  # In this case, the key does not exist so we output empty
-            check_bb_on_cop = True
+        tile_list_cop = load_tiles_list_eo(filename_tiles_cop, "Copernicus")
+        output_cop.append(get_tile(tile_list_cop, tile_id))
+        tile = output_cop[0]
 
-    if not l8_only:
+    if not l8_only and is_s2:
         # Search on s2 Tiles
-        tile_list_s2 = geom_to_s2_tiles(wkt, None, filename_tiles_s2)
-        try:
-            output_s2.append(get_tile_s2(tile_list_s2, tile_id))
-            tile = output_s2[0]
-        except (
-            KeyError,
-            ValueError,
-        ):  # In this case, the key does not exist so we output empty
-            if not s2_only or srtm or cop:
-                check_bb_on_s2 = True
+        tile_list_s2 = load_tiles_list_s2(filename_tiles_s2)
+        output_s2.append(get_tile_s2(tile_list_s2, tile_id))
+        tile = output_s2[0]
+
     try:
-        if check_bb_on_l8 and tile is not None:
+        if tile is not None and not is_l8 and not s2_only:
             output_l8 = create_tiles_list_eo_from_geometry(
                 filename_tiles_l8, tile.polyBB, "L8", min_overlap
             )
-        if check_bb_on_s2 and tile is not None:
+        if tile is not None and not is_s2 and not l8_only:
             output_s2 = create_tiles_list_s2_from_geometry(
                 filename_tiles_s2, tile.polyBB, min_overlap
             )
-        if check_bb_on_srtm and tile is not None:
+        if tile is not None and not is_srtm and srtm:
             output_srtm = create_tiles_list_eo_from_geometry(
                 filename_tiles_srtm, tile.polyBB, "SRTM", min_overlap
             )
-        if check_bb_on_cop and tile is not None:
+        if tile is not None and not is_cop and cop:
             output_cop = create_tiles_list_eo_from_geometry(
                 filename_tiles_srtm, tile.polyBB, "Copernicus", min_overlap
             )
