6671f9adccfc45c87bb9d2e6210973da19999e1d
Mickael Savinaud <mickael.savinaud@c-s.fr>
2021-03-15 21:45:19 +0100

    update version

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, CS Group"
 author = "CS Group"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
